a3b8b7540abb9f5ac804119622dbc2f635e8ec03
slorez <89404685+slorez@users.noreply.github.com>
2021-08-27 00:27:57 +0300

    в

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -11,6 +11,13 @@ namespace ConsoleTest
             Square ShapeSquare = new Square(124);
             Console.WriteLine($"Площадь окружности: {ShapeCircel.GetArea(), -12}");
             Console.WriteLine($"Площадь квадрата: {ShapeSquare.GetArea(), -12}");
+
+            string a = null;
+            string b = "string";
+            Console.WriteLine($"{a?.Length}");
+            Console.WriteLine($"{a?[2]}");
+            Console.WriteLine($"{a?.Length}");
+            Console.WriteLine($"{b?[3]}");
         }
     }
 }
